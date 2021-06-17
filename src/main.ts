44e96199060a5e93a002ca2749e2976d60e5aaa9
wangruilin <bluesbonewong@gmail.com>
2021-06-17 23:42:05 +0800

    表驱动编程消除if-else

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import * as QueryString from "querystring";
 import md5 = require("md5");
 import {appid, appSecret} from "./private";
 
+const errorMap = {
+    52001: "请求超时",
+    52002: "系统错误",
+    52003: "未授权用户"
+};
+
 export const translate = word => {
     const salt = Math.random();
     const sign = md5(appid + word + salt + appSecret);
@@ -40,7 +46,14 @@ export const translate = word => {
 
             const str = Buffer.concat(chunks).toString();
             const obj: BaiduResult = JSON.parse(str);
-            console.log(obj.trans_result[0].dst);
+
+            if (obj.error_code) {
+                console.error(errorMap[obj.error_code] || obj.error_msg);
+                process.exit(2); // 退出当前进程
+            } else {
+                obj.trans_result.forEach(item => console.log(item.dst));
+                process.exit(0); // 退出当前进程
+            }
         });
     });
 
