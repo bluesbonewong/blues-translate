1b2daf1247e2fa0ba464898de58040f762517b49
wangruilin <bluesbonewong@gmail.com>
2021-06-17 22:49:54 +0800

    调用百度翻译API

--- src/cli.ts
+++ src/cli.ts
@@ -1,10 +1,18 @@
 #!/usr/bin/env node
 import {Command} from "commander";
 import * as process from "process";
+import {translate} from "./main";
 
 const program = new Command();
 program.version("0.0.1")
     .name("fy")
-    .usage("<English>");
+    .usage("<English>")
+    .arguments("<English>")
+    .description("输入说明：", {
+        English: "英文单词"
+    })
+    .action(word => {
+        console.log(translate(word));
+    });
 
 program.parse(process.argv); // 解析参数
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,38 @@
-console.log("hi");
+import * as https from "https";
+import * as QueryString from "querystring";
+import md5 = require("md5");
+
+export const translate = word => {
+    const appid = "???", appSecret = "??", salt = Math.random();
+    const sign = md5(appid + word + salt + appSecret);
+
+    const query: String = QueryString.stringify({
+        q: word,
+        from: "en",
+        to: "zh",
+        appid,
+        salt,
+        sign
+    });
+
+    const options = {
+        hostname: "api.fanyi.baidu.com",
+        port: 443,
+        path: `/api/trans/vip/translate?${query}`,
+        method: "GET"
+    };
+
+    const req = https.request(options, (res) => {
+        console.log("statusCode:", res.statusCode);
+        console.log("headers:", res.headers);
+
+        res.on("data", (d) => {
+            process.stdout.write(d);
+        });
+    });
+
+    req.on("error", (e) => {
+        console.error(e);
+    });
+    req.end();
+};
